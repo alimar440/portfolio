dc7e92f610b27ef5aa1d7a74d28e4feccee99205
Ali Mar NGOM <ngomali440@gmail.com>
2025-06-07 20:21:21 +0000

    Commit

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // Check if no token
+  if (!token) {
+    return res.status(401).json({ message: 'Pas de token, autorisation refusée' });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    // Add user from payload
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token non valide' });
+  }
+}; 
